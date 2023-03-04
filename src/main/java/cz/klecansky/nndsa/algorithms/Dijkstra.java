8e4e134d08a9eb27b96a1a302a2a3a00fb867df1
Pavel Klečanský <klecanskypavel@gmail.com>
2023-03-04 12:51:56 +0100

    Fixing DFS.

--- src/main/java/cz/klecansky/nndsa/algorithms/Dijkstra.java
+++ src/main/java/cz/klecansky/nndsa/algorithms/Dijkstra.java
@@ -6,8 +6,10 @@ import cz.klecansky.nndsa.rail.Crossing;
 import cz.klecansky.nndsa.rail.Rail;
 import cz.klecansky.nndsa.rail.RailSwitch;
 import cz.klecansky.nndsa.rail.RailwayInfrastructure;
+import javafx.util.Pair;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Dijkstra {
 
@@ -30,9 +32,9 @@ public class Dijkstra {
                 if (vertex.getPreviosVertex() != null) {
                     if (infrastructure.isPartOfIllegalPath(vertex.getPreviosVertex().getKey(), vertex.getKey(), edge.getTarget().getKey())) {
                         System.out.printf("Illegal Path: %s->%s->%s%n", vertex.getPreviosVertex().getKey(), vertex.getKey(), edge.getTarget().getKey());
-                        Set<Edge<String, RailSwitch, Rail>> reversePath = getReversePath(vertex.getPreviosVertex(), vertex, edge.getTarget(), trainLength);
+                        Stack<Edge<String, RailSwitch, Rail>> reversePath = getReversePath(vertex.getPreviosVertex(), vertex, edge.getTarget(), trainLength, infrastructure);
                         System.out.println(reversePath);
-                        reversePaths.put(vertex.getKey(), reversePath);
+                        reversePaths.put(vertex.getKey(), new HashSet<>(reversePath));
                         double reversePathSum = reversePath.stream().map(Edge::getValue).map(Rail::getLength).mapToDouble(Double::doubleValue).sum();
                         if (reversePathSum < trainLength) {
                             continue;
@@ -88,13 +90,19 @@ public class Dijkstra {
         return crossings;
     }
 
-    private Set<Edge<String, RailSwitch, Rail>> getReversePath(Vertex<String, RailSwitch, Rail> from, Vertex<String, RailSwitch, Rail> currentVertex, Vertex<String, RailSwitch, Rail> destination, double threshold) {
+    private Stack<Edge<String, RailSwitch, Rail>> getReversePath(Vertex<String, RailSwitch, Rail> from, Vertex<String, RailSwitch, Rail> currentVertex, Vertex<String, RailSwitch, Rail> destination, double threshold, RailwayInfrastructure infrastructure) {
         Stack<Vertex<String, RailSwitch, Rail>> stack = new Stack<>();
         Set<Vertex<String, RailSwitch, Rail>> visited = new HashSet<>();
-        Set<Edge<String, RailSwitch, Rail>> moveOver = new HashSet<>();
+        Stack<Edge<String, RailSwitch, Rail>> moveOver = new Stack<>();
+        Stack<Pair<Edge<String, RailSwitch, Rail>, List<Edge<String, RailSwitch, Rail>>>> test = new Stack<>();
+        List<List<Edge<String, RailSwitch, Rail>>> allPaths = new ArrayList<>();
         int totalWeight = 0;
 
+        List<Edge<String, RailSwitch, Rail>> currentList = new ArrayList<>();
+
         stack.push(currentVertex);
+
+        // double remainingThreshold = threshold;
 
         while (!stack.isEmpty()) {
             if (threshold <= totalWeight) {
@@ -112,16 +120,42 @@ public class Dijkstra {
                     if (neighbor.getValue().isTrainNear()) {
                         continue;
                     }
+                    if (threshold <= totalWeight) {
+                        break;
+                    }
+
+//                    if (current.getPreviosVertex() != null) {
+//                        System.out.printf("Illegal Path: %s->%s->%s%n", current.getPreviosVertex().getKey(), current.getKey(), neighbor.getKey());
+//                        if (infrastructure.isPartOfIllegalPath(current.getPreviosVertex().getKey(), current.getKey(), neighbor.getKey())) {
+//                            continue;
+//                        }
+//                    }
                     double edgeWeight = e.getValue().getLength();
 
                     if (!visited.contains(neighbor)) {
+                        System.out.println(e);
+                        currentList.add(e);
+                        allPaths.add(currentList);
+                        currentList = new ArrayList<>(currentList);
                         moveOver.add(e);
                         totalWeight += edgeWeight;
                         stack.push(neighbor);
+
                     }
                 }
             }
         }
+
+//        System.out.println("------- Test -----------");
+//        for (Pair<Edge<String, RailSwitch, Rail>, List<Edge<String, RailSwitch, Rail>>> edgeListPair : test) {
+//            System.out.println(edgeListPair);
+//        }
+//        System.out.println("------------------------");
+        System.out.println("------- Test -----------");
+        for (List<Edge<String, RailSwitch, Rail>> allPath : allPaths) {
+            System.out.println(allPath);
+        }
+        System.out.println("------------------------");
         return moveOver;
     }
 
